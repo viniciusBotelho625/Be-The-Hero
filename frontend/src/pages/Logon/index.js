9bbc3fcc1f7f1b5febc1840cae3fa052a70a84d0
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-03-30 15:42:50 -0300

    Div container e Formulário

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import { FiLogIn} from 'react-icons/fi'
 import './style.css';
+
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
     return (
-        <h1>Hello World</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be-The-hero"/>
+
+                <form>
+                    <h1>Faça seu Login</h1>
+
+                    <input placeholder="Sua ID"/>
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
     );
 }
