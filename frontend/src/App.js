1f1253c0019c33b54e99b808352b83621f2a656d
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-03-30 14:38:18 -0300

    Css Global

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Header from './Header';
+import Logon from './pages/Logon';
 
 function App() {
   return (
-    <Header />
+    <Logon />
   );
 }
-<h1>quero saber oq houve</h1>
+
 export default App;
   
