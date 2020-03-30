8d48734ef7748c8ac93ae7ee050f067692248b7d
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-03-29 21:56:54 -0300

    testbug

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from './Header';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+<h1>quero saber oq houve</h1>
+export default App;
+  
---
+++ front/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Be The Hero</h1>
+        </header>
+    );
+    <h1>e agr?</h1>
+}  
