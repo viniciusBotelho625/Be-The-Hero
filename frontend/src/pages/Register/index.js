cd5fcd71f376007ac20e7463b29741b3e81eb266
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-04-13 15:57:34 -0300

    pagina new incident ✔

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -18,7 +18,7 @@ export default function Register() {
 
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
-                        Não tenho cadastro
+                        Já tenho cadastro
                     </Link>
                 </section>
                 <form>
