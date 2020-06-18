5b282cf5539dfa6a79e490106e86aa362cfe4a5c
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-06-18 19:56:03 -0300

    Tela entre em contato

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,8 +1,47 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+
+import logoImg from '../../assents/logo.png';
+import styles from './styles';
 
 export default function Detail() {
-    return(
-        <View />
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+
+                <TouchableOpacity onPress={() => {}}>
+                    <Feather name="arrow-left" size={28} color="#E02041" />
+                </TouchableOpacity>
+            </View> 
+
+            <View style={styles.incident}>
+                <Text style={styles.incidentProperty}>ONG</Text>
+                <Text style={styles.incidentValeu}>CAMP</Text>
+                
+                <Text style={styles.incidentProperty}>CASO</Text>
+                <Text style={styles.incidentValeu}>Cadelinha atropelada</Text>
+
+                <Text style={styles.incidentProperty}>VALOR</Text>
+                <Text style={styles.incidentValeu}>R$ 120,00</Text>
+            </View>
+
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Salve o dia!</Text>
+                <Text style={styles.heroTitle}>Seja o heroi desse caso.</Text>
+                <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.actions} onPress={() =>{}}>
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.actions} onPress={() =>{}}>
+                        <Text style={styles.actionText}>E-mail</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>  
     );
 }
