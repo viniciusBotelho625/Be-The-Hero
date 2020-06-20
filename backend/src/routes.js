89f8a926a07301b07c5f1dba47fa98671b55b702
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-06-20 19:09:37 -0300

    Validação de cadastro

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { Segments, celebrate, Joi} = require('celebrate');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -10,7 +10,15 @@ const routes = express.Router();
 routes.post('/session', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({ 
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        cidade: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 
 routes.get('/profile', ProfileController.index);
 
